94edd86e5a80c3f4445ed21d27275204570916cd
kemal <kemalyen@gmail.com>
2025-07-25 16:56:43 +0100

    error page is added

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use jeremykenedy\LaravelRoles\App\Exceptions\RoleDeniedException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,5 +19,8 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->report(function (RoleDeniedException $e) {
+
+            return response()->view('errors.403', ['error' => $e->getMessage()], 403);
+        });
     })->create();
---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,25 @@
+<x-layouts.frontend>
+
+
+    <x-slot name="title">
+    403 Forbidden
+    </x-slot>
+    <x-slot name="header">
+        <h2 class="text-lg font-semibold leading-tight text-gray-800 dark:text-gray-200">
+           403 Forbidden
+        </h2>
+    </x-slot> 
+
+    @volt('home.index')
+    <div class="pb-5">
+        <div class="mx-auto space-y-6">
+            <x-card shadow>
+               <h3 class="text-2xl">403 Forbidden</h3>
+               {{$error ?? 'You do not have permission to access this page.'}}
+            </x-card>
+        </div>
+    </div>
+    
+    @endvolt
+
+</x-layouts.frontend>
